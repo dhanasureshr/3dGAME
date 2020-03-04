74201cb04df65fd415c68c120c4d42a496edfdab
dhana suresh <dhanasuresh0002@gmail.com>
2020-03-05 00:19:52 +0530

    small update

--- fight/Assets/scripts/PLAYER/player_joy_movement_irrspective_to_cam.cs
+++ fight/Assets/scripts/PLAYER/player_joy_movement_irrspective_to_cam.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class player_joy_movement_irrspective_to_cam : ExtendedCustomMonoBehavior
 {
-   ///--------------------------------------------------------------------------
-   ///==========================================================================
-   ///--------------------------------------------------------------------------
-   ///this script should be enabled when the player is in the fighting mode
-   ///to give good control of player movement when fighting 
-   ///the main working of this scrpt is 
-   ///by using this script the player can navagete around the scene
-   ///by having a camera at constant rotation means the camera will not consider 
-   ///player rotation 
-   ///--------------------------------------------------------------------------
-   ///==========================================================================
-   ///--------------------------------------------------------------------------
-   
+    ///--------------------------------------------------------------------------
+    ///==========================================================================
+    ///--------------------------------------------------------------------------
+    ///this script should be enabled when the player is in the fighting mode
+    ///to give good control of player movement when fighting 
+    ///the main working of this scrpt is 
+    ///by using this script the player can navagete around the scene
+    ///by having a camera at constant rotation means the camera will not consider 
+    ///player rotation 
+    ///--------------------------------------------------------------------------
+    ///==========================================================================
+    ///--------------------------------------------------------------------------
+
+
+    #region  public variables
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
+    public VirtualJoystick joystickcontroller_ref;
+    #endregion
+
+    #region private variables
+    private float x;
+    private float y;
+    #endregion
+
+
+    #region methods
+    private void Start()
+    {
+        charactercontroller = GetComponent<CharacterController>();
+        tempVEC = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if(charactercontroller.isGrounded)
+        {
+            x = joystickcontroller_ref.InputDirection.x;
+            y = joystickcontroller_ref.InputDirection.z;
+            tempVEC = new Vector3(-y, 0, x);
+            tempVEC = transform.InverseTransformDirection(tempVEC);
+            tempVEC *= speed;
+        }
+
+        tempVEC.y -= gravity * Time.deltaTime;
+        charactercontroller.Move(tempVEC * Time.deltaTime);
+    }
 
 
 
-    
-
-
+    #endregion
 }
