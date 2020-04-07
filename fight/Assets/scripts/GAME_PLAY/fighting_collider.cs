2de976812dbfac8656e19267d42c4b85b326c77e
dhana suresh <dhanasuresh0002@gmail.com>
2020-04-07 11:46:16 +0530

    started basic game play for fighting
    player colliders are created

---
+++ fight/Assets/scripts/GAME_PLAY/fighting_collider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighting_collider : MonoBehaviour
+{
+    [TextArea]
+    public string doc = "this script should be attatched to the player and enimy  collision points";
+
+    /// <summary>
+    /// //this is the main script for handling the player fighting collisions
+    /// </summary>
+    /// <param name="collision"></param>
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Enimy"))
+        {
+            Debug.Log("player colided the enimy");
+        }
+
+        if(collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("Enimy collided  the player");
+        }
+    }
+}
--- fight/Assets/scripts/HELPER/ExtendedCustomMonoBehavior.cs
+++ fight/Assets/scripts/HELPER/ExtendedCustomMonoBehavior.cs
@@ -24,4 +24,6 @@ public class ExtendedCustomMonoBehavior : MonoBehaviour
 	{
 		iD = anId;
 	}
+
+
 }
