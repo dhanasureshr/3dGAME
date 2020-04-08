04d46e22e02655d4188b9316cec7c71428d3f49d
dhana suresh <dhanasuresh0002@gmail.com>
2020-04-08 17:10:07 +0530

    completed player fighting  combo

--- fight/Assets/scripts/PLAYER/PLAYER_ANIMATION_HELPER.cs
+++ fight/Assets/scripts/PLAYER/PLAYER_ANIMATION_HELPER.cs
@@ -1,24 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PLAYER_ANIMATION_HELPER : MonoBehaviour
 {
-    public Animator player_animator_ref;
+    #region combo variables
+    public enum CombeState
+    { 
+        NONE,
+        PUNCH_1,
+        PUNCH_2,
+        PUNCH_3,
+        PUNCH_4,
+        PUNCH_5,
+        PUNCH_6,
+        KICK_1,
+        KICK_2
+    }
 
+    private bool activateTimerToReset;
+    private float default_combo_timer = 0.4f;
+    private float current_combo_timer;
+    private CombeState current_combo_state;
+    #endregion
+
+    #region animation variables
+    public Animator player_animator_ref;
+    // these all are one punch1 sets
     private static int cross_ounch = Animator.StringToHash("CROSS_OUNCH");
-    private static int down_up_punch = Animator.StringToHash("DOWN_UP_PUNCH");
-    private static int forward = Animator.StringToHash("FORWARD");
     private static int hook_punch = Animator.StringToHash("HOOK_PUNCH");
+    private static int forward = Animator.StringToHash("FORWARD");
+   
+    // thes all are one punch2 sets
+    
     private static int punch = Animator.StringToHash("PUNCH");
+    private static int punch_combo = Animator.StringToHash("PUNCH_COMBO");
+    private static int down_up_punch = Animator.StringToHash("DOWN_UP_PUNCH");
+
+    //these all are kick sets
     private static int right_side_kick = Animator.StringToHash("RIGHT_SIDE_KICK");
     private static int round_kick = Animator.StringToHash("ROUND_KICK");
-    private static int punch_combo = Animator.StringToHash("PUNCH_COMBO");
+    
 
+    #endregion
     private void Start()
     {
-       // player_animator_ref = GetComponent<Animator>();
+        current_combo_timer = default_combo_timer;
+        current_combo_state = CombeState.NONE;
     }
+
+    private void Update()
+    {
+        ResetComboState();
+    }
+
+    #region animation functions 
+
     public void PLAY_CROSS_OUNCH()
     {
         player_animator_ref.SetTrigger(cross_ounch);
@@ -55,24 +93,126 @@ public class PLAYER_ANIMATION_HELPER : MonoBehaviour
         player_animator_ref.SetTrigger(round_kick);
     }
 
-
+    #endregion
 
 
     #region prayer fighting functions
     public void PUNCH1_BUTTON_PRESSED()
     {
-        PLAY_FORWARD();
+        if(current_combo_state == CombeState.PUNCH_3 ||
+            current_combo_state == CombeState.PUNCH_4 ||
+            current_combo_state == CombeState.PUNCH_5 ||
+            current_combo_state == CombeState.PUNCH_6 ||
+            current_combo_state == CombeState.KICK_1||
+            current_combo_state == CombeState.KICK_2)
+        {
+            return;
+        }
+
+        current_combo_state++;
+        activateTimerToReset = true;
+        current_combo_timer = default_combo_timer;
+
+        if(current_combo_state == CombeState.PUNCH_1)
+        {
+            PLAY_CROSS_OUNCH();
+        }
+        if(current_combo_state == CombeState.PUNCH_2)
+        {
+            PLAY_HOOK_PUNCH();
+        }
+        if(current_combo_state == CombeState.PUNCH_3)
+        {
+            PLAY_FORWARD();
+        }
     }
 
     public void PUNCH2_BUTTON_PRESSED()
     {
-        PLAY_CROSS_OUNCH();
+        if(current_combo_state == CombeState.PUNCH_6 ||
+            current_combo_state == CombeState.PUNCH_3||
+            current_combo_state == CombeState.KICK_2)
+        {
+            return;
+
+        }
+
+        if(current_combo_state == CombeState.NONE||
+            current_combo_state == CombeState.PUNCH_1||
+            current_combo_state == CombeState.PUNCH_2)
+        {
+            current_combo_state = CombeState.PUNCH_4;
+        }
+        else if(current_combo_state == CombeState.PUNCH_4)
+        {
+            current_combo_state++;
+        }
+        activateTimerToReset = true;
+        current_combo_timer = default_combo_timer;
+
+        if(current_combo_state == CombeState.PUNCH_4)
+        {
+            PLAY_PUNCH();
+
+        }
+        if(current_combo_state == CombeState.PUNCH_5)
+        {
+            PLAY_PUNCH_COMBO();
+        }
+        if(current_combo_state == CombeState.PUNCH_6)
+        {
+            PLAY_DOWN_UP_PUNCH();
+        }
     }
 
     public void KICK_BUTTON_PRESSED()
     {
-        PLAY_ROUND_KICK();
+        if (current_combo_state == CombeState.KICK_2 ||
+              current_combo_state == CombeState.PUNCH_3 ||
+              current_combo_state == CombeState.PUNCH_6)
+        {
+            return;
+        }
+
+        if (current_combo_state == CombeState.NONE ||
+              current_combo_state == CombeState.PUNCH_4 ||
+              current_combo_state == CombeState.PUNCH_5)
+        {
+            current_combo_state = CombeState.KICK_1;
+        }
+        else if(current_combo_state == CombeState.KICK_1)
+        {
+            current_combo_state++;
+        }
+        activateTimerToReset = true;
+        current_combo_timer = default_combo_timer;
+        if(current_combo_state == CombeState.KICK_1)
+        {
+            PLAY_RIGHT_SIDE_KICK();
+        }
+        if(current_combo_state == CombeState.KICK_2)
+        {
+            PLAY_ROUND_KICK();
+        }
     }
     #endregion
 
+
+
+
+    private void ResetComboState()
+    {
+        if (activateTimerToReset)
+        {
+            current_combo_timer -= Time.deltaTime;
+
+
+            if (current_combo_timer <= 0.0f)
+            {
+                current_combo_state = CombeState.NONE;
+                activateTimerToReset = false;
+                current_combo_timer = default_combo_timer;
+            }
+        }
+    }
 }
