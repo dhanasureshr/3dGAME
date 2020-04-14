0dd9234d7d463862c282e2e57eddfbebbfb21536
dhanasuresh <dhanasoftwares@email.com>
2020-04-14 15:17:20 +0530

    added enimy_movement_script to enimy

---
+++ fight/Assets/scripts/enimy/enimy_movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enimy_movement : MonoBehaviour
+{
+    /// <summary>
+    /// In this script manages the enimy movement and the related handling like 
+    /// determing state of the enimy like 
+    /// fighting state
+    /// petrolling state
+    /// chasing state
+    /// </summary>
+    [TextArea]
+    public string objd = "this should be attatched to the enimy mesh where enimy animator and nav_mesh_agent";
+
+}
