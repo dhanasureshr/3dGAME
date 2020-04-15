63cd2c917fdd5d15d8a3f2ba24f918553c0e3e7a
dhana suresh <dhanasuresh0002@gmail.com>
2020-04-15 12:35:55 +0530

    enimy movement started

--- fight/Assets/scripts/enimy/enimy_movement.cs
+++ fight/Assets/scripts/enimy/enimy_movement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class enimy_movement : MonoBehaviour
+public class enimy_movement : ExtendedCustomMonoBehavior
 {
     /// <summary>
     /// In this script manages the enimy movement and the related handling like 
@@ -13,5 +14,35 @@ public class enimy_movement : MonoBehaviour
     /// </summary>
     [TextArea]
     public string objd = "this should be attatched to the enimy mesh where enimy animator and nav_mesh_agent";
+
+    public NavMeshAgent enimy_nav_mesh_agent;
+    public Transform target_position;
+    public float move_speed;
+    public float min_distance;
+    public float max_distance;
+
+    public bool patrol=false;
+    public bool chase=false;
+    public bool fight=false;
+
+
+    private void Update()
+    {
+        tempVEC = transform.position - target_position.position;
+        move_enimy();
+    }
+
+    public void  move_enimy()
+    {
+        
+           
+            enimy_nav_mesh_agent.SetDestination(target_position.position);
+        
+
+
+        
+    }
+
+
 
 }
