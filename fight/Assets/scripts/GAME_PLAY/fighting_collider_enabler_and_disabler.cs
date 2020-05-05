71e938cf42cf58a40f325ae72b54d16785bdd5ee
dhana suresh <dhanasuresh0002@gmail.com>
2020-05-05 21:07:00 +0530

    common enimy logic started

--- fight/Assets/scripts/GAME_PLAY/fighting_collider_enabler_and_disabler.cs
+++ fight/Assets/scripts/GAME_PLAY/fighting_collider_enabler_and_disabler.cs
@@ -9,10 +9,16 @@ public class fighting_collider_enabler_and_disabler : MonoBehaviour
     public GameObject LEFT_HAND_COLLIDER;
     public GameObject RIGHT_LEG_COLLIDER;
     public GameObject LEFT_LEG_COLLIDER;
-    
 
+    private void Start()
+    {
+        RIGHT_LEG_COLLIDER = transform.Find("RIGHT__LEG_HIT_COLLIDER").gameObject;
+        LEFT_LEG_COLLIDER = transform.Find("LEFT__LEG_HIT_COLLIDER").gameObject;
+        RIGHT_HAND_COLLIDER = transform.Find("RIGHT__HAND_HIT_COLLIDER").gameObject;
+        LEFT_HAND_COLLIDER = transform.Find("LEFT__HAND_HIT_COLLIDER").gameObject;
+    }
     // right hand 
-     void RIGHT_HAND_ENABLER()
+    void RIGHT_HAND_ENABLER()
     {
         RIGHT_HAND_COLLIDER.SetActive(true);
     }
