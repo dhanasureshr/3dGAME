9f1015dd5d17c36edd231e1e7973022d08cf0306
dhana suresh <dhanasuresh0002@gmail.com>
2020-07-08 05:30:39 +0530

    updated sword logic

---
+++ fight/Assets/scripts/PLAYER/sword scripts/sword.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword : ExtendedCustomMonoBehavior
+{
+
+    public float sword_applialbe_damage = 10.0f;
+    public GameObject blood_impact_prefab;
+
+    //public void OnCollisionEnter(Collision colision)
+    //{
+    //    if (colision.transform.CompareTag(tags.full_enimy_tag))
+    //    {
+    //        colision.transform.gameObject.GetComponentInParent<health>().ApplyDamage(sword_applialbe_damage, false);
+    //        Instantiate(blood_impact_prefab, transform.position, Quaternion.LookRotation(colision.contacts[0].normal));
+    //        Debug.Log("sword splashed the enimy ok dhana");
+    //    }
+    //}
+
+    public void OnTriggerEnter(Collider colision)
+    {
+
+        if (colision.transform.CompareTag(tags.full_enimy_tag))
+        {
+            colision.transform.gameObject.GetComponentInParent<health>().ApplyDamage(sword_applialbe_damage, false);
+            Instantiate(blood_impact_prefab, transform.position,transform.rotation);
+            Debug.Log("sword splashed the enimy ok dhana");
+        }
+    }
+
+
+
+}
