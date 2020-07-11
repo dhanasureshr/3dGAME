c34d2f5ac61dfb3e5240d72b45ab02f5cffa2707
dhana suresh <dhanasuresh0002@gmail.com>
2020-07-11 12:20:46 +0530

    updated sword particles collision logic

---
+++ fight/Assets/sword_plaer_collision_detection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword_plaer_collision_detection : MonoBehaviour
+{
+    public float sword_player_partical_applible_damage = 0.1f;
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.CompareTag(tags.full_enimy_tag))
+        {
+
+            other.transform.gameObject.GetComponent<health>().ApplyDamage(sword_player_partical_applible_damage, false);
+        }
+    }
+}
