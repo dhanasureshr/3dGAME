53f805c43d849870409aa21922ba04178d2a56b0
dhana suresh <dhanasuresh0002@gmail.com>
2020-07-14 14:45:38 +0530

    added the magical vfsx

---
+++ fight/Assets/scripts/PLAYER/magic_scripts/magic_particls_prefab_attacker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magic_particls_prefab_attacker : MonoBehaviour
+{
+    public GameObject[] H1_EFFECTER_PREFABS;
+    public GameObject[] H2_EFFECTER_PREFABS;
+    public GameObject[] h1_CHARGE_PREFABS;
+    public GameObject[] h2_CHARGE_PREFABS;
+    public GameObject[] practical_hit_PREFABS;
+
+
+
+    public Transform ONE_HAND_CONTAINER;
+    public Transform TWO_HAND_CONTAINER;
+
+    public void intantiate_one_hand_partical_prefab()
+    {
+
+    }
+
+    public void instantiate_two_hand_partical_prefab()
+    {
+
+
+    }
+
+    public IEnumerator instantiate_partical_effector_prefab()
+    {
+
+
+        yield break;
+
+    }
+
+    public IEnumerator instantiate_partical_hit_prefab()
+    {
+        
+
+        yield break;
+
+    }
+    public void intantiate_two_hand_partical_prefab()
+    {
+
+
+    }
+}
