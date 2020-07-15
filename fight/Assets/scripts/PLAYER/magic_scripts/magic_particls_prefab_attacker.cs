4ea182b01715b26491502c62d8567534a01f5ef0
dhana suresh <dhanasuresh0002@gmail.com>
2020-07-15 14:06:24 +0530

    tryed to add magical 2h effects

--- fight/Assets/scripts/PLAYER/magic_scripts/magic_particls_prefab_attacker.cs
+++ fight/Assets/scripts/PLAYER/magic_scripts/magic_particls_prefab_attacker.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class magic_particls_prefab_attacker : MonoBehaviour
 {
     public Transform[] H1_EFFECTER_PREFABS;
-    public GameObject[] H2_EFFECTER_PREFABS;
-    public GameObject[] h1_CHARGE_PREFABS;
-    public GameObject[] h2_CHARGE_PREFABS;
+    public Transform[] H2_EFFECTER_PREFABS;
+    public Transform[] h1_CHARGE_PREFABS;
+    public Transform[] h2_CHARGE_PREFABS;
     public GameObject[] practical_hit_PREFABS;
 
     
@@ -15,18 +15,25 @@ public class magic_particls_prefab_attacker : MonoBehaviour
     public Transform ONE_HAND_CONTAINER;
     public Transform TWO_HAND_CONTAINER;
 
-    public void intantiate_one_hand_partical_prefab()
+    public void intantiate_one_hand_partical_prefab(int n)
     {
-      var magic_partical =(Transform) Instantiate(H1_EFFECTER_PREFABS[Random.Range(0, 3)], ONE_HAND_CONTAINER.position, Quaternion.identity);
+        //var magic_partical =(Transform) Instantiate(H1_EFFECTER_PREFABS[Random.Range(0, 3)], ONE_HAND_CONTAINER.position, Quaternion.identity);
+        
+        Transform  magic_partical = partical_instantiator(H1_EFFECTER_PREFABS, n);
+
         magic_partical.GetComponent<Rigidbody>().velocity = transform.forward * 25;
-        StartCoroutine(destroy_partical(magic_partical));
+        StartCoroutine(destroy_partical(magic_partical,2));
 
     }
 
-    public void instantiate_two_hand_partical_prefab()
+    public void instantiate_two_hand_partical_prefab(int n)
     {
-
-
+        Transform magic_partical = partical_instantiator(h2_CHARGE_PREFABS, n);
+        magic_partical.rotation = TWO_HAND_CONTAINER.rotation;
+        if (n == 0)
+        {
+            StartCoroutine(destroy_partical(magic_partical, 2.0f));
+        }
     }
 
     public IEnumerator instantiate_partical_effector_prefab()
@@ -45,10 +52,44 @@ public class magic_particls_prefab_attacker : MonoBehaviour
 
     }
 
-    public IEnumerator destroy_partical(Transform pratical_to_destroy)
+    public IEnumerator destroy_partical(Transform pratical_to_destroy,float time_delay)
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(time_delay);
         Destroy(pratical_to_destroy.gameObject);
         yield break;
     }
+
+
+    public Transform  partical_instantiator(Transform[] h1_list,int effect_number)
+    {
+        var magic_partical = (Transform)Instantiate(h1_list[effect_number], TWO_HAND_CONTAINER.position, Quaternion.identity);
+        
+        return magic_partical;
+    }
+
+
+
+    public void Partical_attack_one_HAND1()
+    {
+        intantiate_one_hand_partical_prefab(0);
+    }
+
+    public void Partical_attack_one_HAND2()
+    {
+        intantiate_one_hand_partical_prefab(1);
+    }
+
+
+    public void Partical_attack_one_HAND3()
+    {
+        intantiate_one_hand_partical_prefab(2);
+    }
+
+
+    public void Partical_attack_two_HAND1()
+    {
+        instantiate_two_hand_partical_prefab(0);
+    }
+
+
 }
