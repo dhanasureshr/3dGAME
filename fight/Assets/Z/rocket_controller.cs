4e49157d685078fd29ab6e0455a5b96c1b653288
dhana suresh <dhanasuresh0002@gmail.com>
2020-08-10 00:43:37 +0530

    started rocket game play

---
+++ fight/Assets/Z/rocket_controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket_controller : MonoBehaviour
+{
+    public Animator rocket_animator;
+
+    public static int rocket_fire = Animator.StringToHash("lauch_rocket");
+
+    public void Start()
+    {
+        rocket_animator = gameObject.GetComponent<Animator>();
+
+    }
+
+    public void PLAY_PLAYER_ROCKET_FIRE_ANIMATION()
+    {
+        rocket_animator.SetTrigger(rocket_fire);
+    }
+
+    public void Fire_Rocket()
+    {
+
+    }
+}
