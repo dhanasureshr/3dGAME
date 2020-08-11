0517f2e7f664a71e3c1677e5f00f4afea8874155
dhana suresh <dhanasuresh0002@gmail.com>
2020-08-11 14:50:41 +0530

    added oil barrel explode effect

---
+++ fight/Assets/Z/oil_barrel_explode_script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oil_barrel_explode_script : MonoBehaviour
+{
+    public Transform destroyed_barrel;
+    //public Transform explode_vfx;
+
+   // public rocket t;
+
+    private void OnTriggerEnter(Collider other)
+    {
+      //  t.explode = true;
+        Instantiate(destroyed_barrel, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
