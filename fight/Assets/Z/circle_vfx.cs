f286605738551612b8ce9f89a959b60bc91085cd
dhana suresh <dhanasuresh0002@gmail.com>
2020-08-18 22:14:43 +0530

    vfx work started

---
+++ fight/Assets/Z/circle_vfx.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+public class circle_vfx : MonoBehaviour
+{
+
+    public VisualEffect tss;
+
+    int event_id = Shader.PropertyToID("entered");
+    int stop_id = Shader.PropertyToID("exit");
+   public void Start()
+    {
+        tss = gameObject.GetComponent<VisualEffect>();
+ 
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(tags.full_player_tag))
+        {
+
+            tss.SendEvent(event_id);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag(tags.full_player_tag))
+        {
+            tss.SendEvent(stop_id);
+        }
+    }
+
+}
