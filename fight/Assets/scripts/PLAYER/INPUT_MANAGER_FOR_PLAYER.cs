c32be09cf25473f6540c79b218ea8bab11618c40
dhana suresh <dhanasuresh0002@gmail.com>
2021-02-15 05:39:33 +0530

    created  player input manger script
    this script helps to mange input for multiple devices

---
+++ fight/Assets/scripts/PLAYER/INPUT_MANAGER_FOR_PLAYER.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+
+public class INPUT_MANAGER_FOR_PLAYER : ExtendedCustomMonoBehavior
+{
+    
+
+
+    /*
+
+     this is the script which helps to provide or connect different types of input devices
+     to player by depending up on the platform we are in 
+
+    */
+
+
+public void Fire()
+    {
+        Debug.Log("player is firing");
+    }
+
+}
