04fb0d5e4d0d56243e3600aa29c5ae2e0dde2240
dhana suresh <dhanasuresh0002@gmail.com>
2021-03-31 12:54:53 +0530

    ok

---
+++ fight/Assets/scripts/PLAYER/player_movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    private Vector3 MoveDirection;
+    private Vector3 ProjectedCameraForward;
+
+    private Quaternion rotationToCamera;
+
+    private Quaternion rotationToMoveDirection;
+
+    [Inject(injectFrom.Anywhere)]
+
+    
+
+    private void Update()
+    {
+        MoveDirection  = Vector3.forward * 
+    }
+}
