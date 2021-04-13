389a2ce596ff17d638ba79dca6e7a881615110dd
dhana suresh <dhanasuresh0002@gmail.com>
2021-04-13 08:40:23 +0530

    dds

---
+++ fight/Assets/scripts/NETWORKING/NETWORKING/NET_TEST_SCRIPTS/Game_Manager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace com.dhanasoftwares
+{
+    
+
+    public class Game_Manager : MonoBehaviourPunCallbacks
+    {
+        #region Photon callbacks
+        
+
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}",other.NickName);
+
+            if(PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}",PhotonNetwork.IsMasterClient);
+
+
+                LoadArena();
+
+            }
+        }
+
+
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}",other.NickName);
+
+            if(PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}",PhotonNetwork.IsMasterClient);
+
+
+                LoadArena();
+            }
+
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if(!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork: Trying to load a level but we are not the master client");
+
+            }
+            Debug.LogFormat("PhotoNetwork : Loading Level :{0}", PhotonNetwork.CurrentRoom.PlayerCount);
+
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+
+        #endregion
+
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+
+    }
+}
---
+++ fight/Assets/scripts/NETWORKING/NETWORKING/NET_TEST_SCRIPTS/Launcher.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    string gameVersion = "1";
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    [SerializeField]
+    private GameObject controlPanel;
+
+    [SerializeField]
+    private GameObject progressLabel;
+
+    bool isConnecting;
+
+
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+    }
+
+    void Start()
+    {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+    }
+
+    public void Connect()
+    {
+
+
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+
+        }
+        else
+        {
+           isConnecting =  PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion  = gameVersion;
+
+        }
+    }
+
+
+
+    #region  MonoBehaviourPunCallbacks callbacks
+    public override void OnConnectedToMaster()
+    {
+        if(isConnecting)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        isConnecting = false;
+
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+        Debug.LogWarningFormat("PNU Basics Tutorial/Lanuncer: ONDisconnected() was called by PUN with reason {0}",cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: ONJoinRandomFailed() was Called by PUN. No random room available ,so create one. \n Calling: PhotonNetwork.CreateRoom");
+
+
+        PhotonNetwork.CreateRoom(null,new RoomOptions{MaxPlayers = maxPlayersPerRoom});
+
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PNU Basics Tutorials/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room ");
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the 'Room for 1' ");
+
+            PhotonNetwork.LoadLevel(1);
+        }
+        
+    }
+
+    #endregion
+}
