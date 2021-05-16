b7802776a63f52ad8b77ea9ed95b41b96325ba4f
dhana suresh <dhanasuresh0002@gmail.com>
2021-05-17 03:20:41 +0530

    completed player jump code

--- fight/Assets/scripts/NET_SCRIPTS/N_Player_scripts/player_movement.cs
+++ fight/Assets/scripts/NET_SCRIPTS/N_Player_scripts/player_movement.cs
@@ -1,55 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class player_movement : MonoBehaviour
+namespace com.dhanasoftwares
 {
-
-    [Inject(InjectFrom.Anywhere)]
-    public INPUT_MANAGER_FOR_PLAYER Player_Input;
-   
-    private Vector3 MoveDirection = Vector3.zero;
-
-    private CharacterController playercharactercontroller;
-    private float rotate_speed = 5.0f;
-
-	private float verticalvelocity;
-
-	private float gravity = 20.8f;
-    public float speed;
-
-    private Vector3 moveVector;
-
-    
-	float angle;
-	Quaternion targetrotation;
-
-    public bool wepon__movement;
-    
-    Animator Networked_Player_animatior;
-
-
-
-    private void Start()
+    public class player_movement : MonoBehaviour
     {
-        playercharactercontroller = GetComponent<CharacterController>();
-        Networked_Player_animatior = GetComponentInChildren<Animator>();
 
-
-    }
-
+        [Inject(InjectFrom.Anywhere)]
+        public INPUT_MANAGER_FOR_PLAYER Player_Input;
     
-    private void Update()
-    {
-        if(wepon__movement)// this condition enables player wepon movement
+        private Vector3 MoveDirection = Vector3.zero;
+
+        private CharacterController playercharactercontroller;
+        private float rotate_speed = 5.0f;
+
+        private float verticalvelocity;
+
+        private float gravity = 20.8f;
+        public float speed;
+
+        private Vector3 moveVector;
+
+        
+        float angle;
+        Quaternion targetrotation;
+
+        public bool wepon__movement;
+        
+        Animator Networked_Player_animatior;
+
+        private float jumpforce = 15.0f;
+
+        private void Start()
         {
-            Networked_Player_animatior.SetLayerWeight(0,1);
-            Networked_Player_animatior.SetLayerWeight(1,0);
-            MoveDirection = Player_Input.moveVec;
-            
+            playercharactercontroller = GetComponent<CharacterController>();
+            Networked_Player_animatior = GetComponentInChildren<Animator>();
+        }
+
+        
+        private void Update()
+        {
+            if(wepon__movement)// this condition enables player wepon movement
+            {
+                Networked_Player_animatior.SetBool("P_wepon_mode",true);
+                MoveDirection = Player_Input.moveVec;
                 MoveDirection = Camera.main.transform.TransformDirection(MoveDirection);
-                MoveDirection = MoveDirection * speed;// here aplying the movement input when player is in middle of jump
-                verticalvelocity -= gravity * Time.deltaTime;
+                //---jump code TO--DO
+                verticalvelocity = jump_motor();
                 angle += Camera.main.transform.eulerAngles.y; // new below comment
                 targetrotation = Quaternion.Euler(0, angle, 0);
                 angle = Mathf.Atan2(Player_Input.moveVec.x,Player_Input.moveVec.z);                         
@@ -58,37 +55,61 @@ public class player_movement : MonoBehaviour
                 targetrotation = Quaternion.Euler(0, angle, 0);
                 transform.rotation = Quaternion.Lerp(transform.rotation,targetrotation,speed  * Time.deltaTime);
                 moveVector = new Vector3(MoveDirection.x,verticalvelocity,MoveDirection.z);
-                playercharactercontroller.Move(moveVector* Time.deltaTime); // this is the move the player
-                Networked_Player_animatior.SetFloat("wepon_inputX",Player_Input.moveVec.x);
-                Networked_Player_animatior.SetFloat("wepon_inputY",Player_Input.moveVec.z);
-            
-        }
-        else // this condition enables player advanture movemnt
-        {
-            
-            Networked_Player_animatior.SetLayerWeight(0,0);
-            Networked_Player_animatior.SetLayerWeight(1,1);
-            // TO---DO 
-            Vector3 direction = new Vector3(Player_Input.moveVec.x,0f,Player_Input.moveVec.z).normalized;
-            if(direction.magnitude >= 0.1f)
-            {
-                float tragetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg  + Camera.main.transform.eulerAngles.y;
-                transform.rotation = Quaternion.Euler(0f,tragetAngle,0f);
-                Vector3 moveDir = Quaternion.Euler(0f,tragetAngle,0f) * Vector3.forward;
-                playercharactercontroller.Move(moveDir.normalized * speed * Time.deltaTime);
-                    
+                playercharactercontroller.Move(moveVector* Time.deltaTime); // this is the move the player    
             }
-            
+            else // this condition enables player advanture movemnt
+            {
+                Networked_Player_animatior.SetBool("P_wepon_mode",false);
+                MoveDirection = Player_Input.moveVec.normalized;
+                if(MoveDirection.magnitude >= 0.1f)
+                {
+                    float tragetAngle = Mathf.Atan2(MoveDirection.x,MoveDirection.z) * Mathf.Rad2Deg  + Camera.main.transform.eulerAngles.y;
+                    transform.rotation = Quaternion.Euler(0f,tragetAngle,0f);
+                    MoveDirection = Quaternion.Euler(0f,tragetAngle,0f) * Vector3.forward;
+                    //---jump code
+                    verticalvelocity = jump_motor();
+                    moveVector = new Vector3(MoveDirection.x,verticalvelocity,MoveDirection.z);
+                    playercharactercontroller.Move(moveVector.normalized * speed * Time.deltaTime);     
+                }
+                                
+            }
             Networked_Player_animatior.SetFloat("wepon_inputX",Player_Input.moveVec.x);    
             Networked_Player_animatior.SetFloat("wepon_inputY",Player_Input.moveVec.z);
 
-            
-
-            
-            //MoveDirection.y -= gravity * Time.deltaTime;
+                
         }
-        
-    }
-    
+        private float jump_motor()
+        {
+             if(playercharactercontroller.isGrounded)
+			        {
+				        verticalvelocity = -gravity * Time.deltaTime;
+				        MoveDirection = MoveDirection * speed; // applying movement when player is on ground
+				        if(Player_Input.jump) // multiplat_form_input_manager.jump 
+				        {
+                            if(wepon__movement)
+                            {
+					            Networked_Player_animatior.SetTrigger("idle_jump");
+					            verticalvelocity = jumpforce;
+                            }
+                            else
+                            {
+					            Networked_Player_animatior.SetTrigger("run_jump");
+					            verticalvelocity = jumpforce + 5;
+                            }
 
+					        Player_Input.jump = false;//multiplat_form_input_manager.jump = false;
+				        }
+			        }
+			        else
+			        {
+				        MoveDirection = MoveDirection * speed;// here aplying the movement input when player is in middle of jump
+				        verticalvelocity -= gravity * Time.deltaTime;
+			        }
+                    
+            return verticalvelocity;
+        }
+
+    } // BY DHANA SURESH -------------->dhanasoftwares@email.com
+
+    
 }
