03e0fb1ee2f69e941ddf0143c8d945958cefe318
dhana suresh <dhanasuresh0002@gmail.com>
2021-06-06 13:09:36 +0530

    player weapon equip update

---
+++ fight/Assets/scripts/NET_SCRIPTS/N_Weapon_scripts/Weapon_pickup_script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_pickup_script : MonoBehaviour
+{
+  public GameObject Weapon_Prefab;
+
+ private void OnTriggerEnter(Collider other)
+ {
+     
+ }
+}
